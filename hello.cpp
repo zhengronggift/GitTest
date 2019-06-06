f0d7e2179d8748ca9a1d15e6a4e8a58ec7f5396d
rong <rzheng@fau.edu>
2019-06-06 13:20:23 -0400

    This is first commit

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+            cout<< "Hello World,\nWelcome to my first C ++ program on Ubuntu Linux" << endl;
+
+            return 0;
+
+}
